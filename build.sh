8a95154a658aebe2117e5c35391322ae5f5243ae
vincent <vincent.ventalon.pro@gmail.com>
2025-08-19 13:00:36 +0700

    ok

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 # Build for iOS
 npx eas build --platform ios --profile production --auto-submit
 
+npx eas build --platform ios --profile production --local
+xcrun altool --upload-app --type ios --file dist/<nom_du_fichier>.ipa --username "tonAppleID" --password "@keychain:AppStoreConnect"
+npx eas submit --platform ios --path
+    monapp.ipa
